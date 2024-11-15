70d3d94f70b62feb8a8e4369413247322e24b0ed
Hananditya Kenanta Setyaputra <hanandityakenanta@gmail.com>
2024-11-15 11:52:40 +0700

    add-controller user dan role, add update user dan delete user, add create role dan delete role

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Role;
+use Illuminate\Support\Facades\Auth;
+
+
+class RoleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $admin = Auth::guard('admin')->user();
+
+        $roles = Role::all();
+
+        return view('admin.crud-role', compact('admin', 'roles'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $admin = Auth::guard('admin')->user();
+
+        return view('admin.create-role', compact('admin'));
+    }
+    
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+        ]);
+    
+        Role::create([
+            'name' => $request->name,
+        ]);
+    
+        return redirect()->route('admin.role.index')->with('success', 'Role berhasil ditambahkan.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $role = Role::findOrFail($id);
+
+        $role->delete();
+
+        return redirect()->route('admin.role.index')->with('success', 'Role berhasil dihapus.');
+    }
+}
--- resources/views/admin/create-user.blade.php
+++ resources/views/admin/create-user.blade.php
@@ -5,18 +5,7 @@
 <div class="container mx-auto p-4">
     <div class="bg-white shadow-md rounded-lg p-6">
         <h2 class="text-2xl text-blue-500 font-semibold mb-6">Tambah Data User</h2>
-        @if (session('success'))
-            <div class="flex items-center p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50" role="alert">
-                <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
-                </svg>
-                <span class="sr-only">Info</span>
-                <div>
-                    <span class="font-medium">{{ session('success') }}</span>
-                </div>
-            </div>
-        @endif
-        <form action="{{route('create.user')}}" method="POST">
+        <form action="{{route('admin.user.store')}}" method="POST">
             @csrf
             <div class="mb-4">
                 <label for="username" class="block text-gray-700">
@@ -25,7 +14,18 @@
                 </label>
                 <input type="text" id="username" name="username" class="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Masukkan username" required>
             </div>
-
+            <div class="mb-4">
+                <label for="role" class="block text-gray-700">
+                    Role
+                    <span class="text-red-500 font-semibold">*</span> 
+                </label>
+                <select id="role" name="role" class="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
+                    <option value="" disabled selected>Pilih Role</option>
+                    @foreach ($roles as $role)
+                        <option value="{{ $role->id }}">{{ $role->name }}</option>
+                    @endforeach
+                </select>
+            </div>
             <div class="mb-4">
                 <label for="password" class="block text-gray-700">
                     Password
